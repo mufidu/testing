efbf0ba27e10708ea34124e499bfe511759265f8
Muhammad Mufid <mufidu@outlook.com>
2021-08-15 21:42:19 +0700

    gaa gcs

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 1
 2
-
+say hello to vim!
---
+++ node/app.js
@@ -0,0 +1,9 @@
+const config = require("./config.js");
+const twitter = require("twitter-lite");
+const client = new twitter(config);
+client
+    .post("statuses/update", { status: "Hello world!" })
+    .then((result) => {
+        console.log('You successfully tweeted this : "' + result.text + '"');
+    })
+    .catch(console.error);
